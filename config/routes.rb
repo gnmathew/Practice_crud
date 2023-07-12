7a5969e4ebac99d261ee19458a060f92639f887e
gnmathew <genemathewdorado@gmail.com>
2023-07-12 14:51:45 +0800

    generated categories controller and created categories routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,6 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   root 'welcome#index'
+
+  resources :categories, except: :show
 end
